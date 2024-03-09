b11c9d7c77e33b8f04e9580b267cb366440ed95d
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-03-10 02:27:32 +0600

    added html

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form>
+        <label>Title</label>
+        <input type="text" />
+        <label>Amount</label>
+        <input type="number" />
+        <label>
+          <input type="radio" />
+          Income
+        </label>
+        <br />
+        <label>
+          <input type="radio" />
+          Expense
+        </label>
+        <button>Submit</button>
+      </form>
+      <div>
+        <p>Total income: </p>
+        <p>Total expense: </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
