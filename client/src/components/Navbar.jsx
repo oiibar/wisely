8a825b0c0b395af7acdc1f3ee3da205e93dff220
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-03-13 04:58:42 +0600

    f

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -58,9 +58,8 @@ function Navbar() {
             <li>
               <Link
                 to="/"
-                exact
-                activeClassName="bg-blue-700 text-white"
-                className="block py-2 px-3 rounded md:p-0 md:dark:text-blue-500"
+                activeClassName="bg-gray-100 text-gray-100"
+                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Home
               </Link>
