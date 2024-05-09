169cd31fb8023c5c6cce201e16a1f8cb80b8538d
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-05-09 14:22:40 +0500

    additional guard

--- server/src/transaction/transaction.module.ts
+++ server/src/transaction/transaction.module.ts
@@ -3,10 +3,12 @@ import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { Transaction } from './entities/transaction.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from 'src/category/entities/category.entity';
+import { CategoryService } from 'src/category/category.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [TypeOrmModule.forFeature([Transaction, Category])],
   controllers: [TransactionController],
-  providers: [TransactionService],
+  providers: [TransactionService, CategoryService],
 })
 export class TransactionModule {}
