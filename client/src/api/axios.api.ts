4e2b07bc320e6cc8fd7f0fb48531ecba2e6ec015
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-10-09 10:05:46 +0500

    deploy

--- client/src/api/axios.api.ts
+++ client/src/api/axios.api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getToken } from "../helpers/localstorage.helper";
 
 export const instance = axios.create({
-  baseURL: "https://wisely-serv.onrender.com/api",
+  // baseURL: "https://wisely-serv.onrender.com/api",
+  baseURL: "https://wisely-production.up.railway.app/api",
   headers: {
     Authorization: "Bearer " + getToken(),
   },
